0d9d7e7837bc438f48ec7a1baba7aebd54ab839f
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-05-09 07:27:20 +0300

    Updated RelayCommand to async type and implement try catch exceptions hadler to Execute method.

---
+++ Finance_Manager_WPF_Front/ViewModels/AsyncRelayCommand.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Input;
+
+namespace Finance_Manager_WPF_Front.ViewModels;
+
+public class AsyncRelayCommand : ICommand
+{
+    private readonly Func<object, Task> _execute;
+    private readonly Func<object, bool> _canExecute;
+    private bool _isExecuting;
+    public AsyncRelayCommand(Func<object, Task> execute, Func<object, bool> canExecute = null)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object parameter)
+    {
+        return !_isExecuting && (_canExecute?.Invoke(parameter) ?? true);
+    }
+
+    public async void Execute(object parameter)
+    {
+        _isExecuting = true;
+        RaiseCanExecuteChanged();
+
+        try
+        {
+            await _execute(parameter);
+        }
+        catch (Exception ex) { }
+        finally
+        {
+            _isExecuting = false;
+            RaiseCanExecuteChanged();
+        }
+    }
+
+    public event EventHandler CanExecuteChanged;
+
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
