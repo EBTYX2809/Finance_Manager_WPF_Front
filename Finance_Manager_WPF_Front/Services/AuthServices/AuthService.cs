e2bf48b56f95e708c8233e1e181fdc9847793f98
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-05-09 07:25:02 +0300

    Created services for authorization and tokens managment.

---
+++ Finance_Manager_WPF_Front/Services/AuthServices/AuthService.cs
@@ -0,0 +1,99 @@
+﻿using AutoMapper;
+using Finance_Manager_WPF_Front.BackendApi;
+using Finance_Manager_WPF_Front.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance_Manager_WPF_Front.Services.AuthServices;
+
+public class AuthService
+{
+    private readonly FinanceApiClient _apiClient;
+    private readonly ApiWrapper _apiWrapper;
+    private readonly UserSession _userSession;    
+    private readonly TokensManager _tokensManager;
+    private readonly IMapper _mapper;
+
+    public AuthService(FinanceApiClient apiClient, UserSession userSession, IMapper mapper, TokensManager tokensManager, ApiWrapper apiWrapper)
+    {
+        _apiClient = apiClient;
+        _userSession = userSession;
+        _mapper = mapper;
+        _tokensManager = tokensManager;
+        _apiWrapper = apiWrapper;
+    }
+
+    public async Task RegisterUser(string email, SecureString securePassword)
+    {
+        string password = null;
+        var ptr = IntPtr.Zero;
+
+        try
+        {
+            ptr = Marshal.SecureStringToBSTR(securePassword);
+            password = Marshal.PtrToStringBSTR(ptr);
+
+            var registerResponse = await _apiWrapper.ExecuteAsync(async () => 
+            await _apiClient.RegisterAsync(new AuthDataDTO { Email = email, Password = password }));
+
+            //if (registerResponse == null) throw new InvalidOperationException();
+
+            _userSession.CurrentUser = _mapper.Map<UserModel>(registerResponse.UserDTO);
+            _userSession.AccessToken = registerResponse.AccessJwtToken;
+            _tokensManager.SaveRefreshToken(registerResponse.RefreshToken);
+        }
+        catch (Exception ex) { throw; }
+        finally
+        {
+            if(ptr != IntPtr.Zero) Marshal.ZeroFreeBSTR(ptr);
+
+            if (password != null) password = null;
+        }                       
+    }
+
+    public async Task AuthUser(string email, SecureString securePassword)
+    {
+
+        string password = null;
+        var ptr = IntPtr.Zero;
+
+        try
+        {
+            ptr = Marshal.SecureStringToBSTR(securePassword);
+            password = Marshal.PtrToStringBSTR(ptr);
+
+            var registerResponse = await _apiWrapper.ExecuteAsync(async () =>
+            await _apiClient.AuthenticateAsync(new AuthDataDTO { Email = email, Password = password }));
+
+            //if (registerResponse == null) throw new InvalidOperationException();
+
+            _userSession.CurrentUser = _mapper.Map<UserModel>(registerResponse.UserDTO);
+            _userSession.AccessToken = registerResponse.AccessJwtToken;
+            _tokensManager.SaveRefreshToken(registerResponse.RefreshToken);
+        }
+        catch (Exception ex) { throw; }
+        finally
+        {
+            if (ptr != IntPtr.Zero) Marshal.ZeroFreeBSTR(ptr);
+
+            if (password != null) password = null;
+        }
+    }
+
+    public async Task LoginUserWithRefreshToken(string refreshToken)
+    {
+        var registerResponse = await _apiWrapper.ExecuteAsync(async () =>
+        await _apiClient.RefreshTokenAsync(refreshToken));
+
+        //if (registerResponse == null) throw new InvalidOperationException(); 
+
+        _userSession.CurrentUser = _mapper.Map<UserModel>(registerResponse.UserDTO);
+        _userSession.AccessToken = registerResponse.AccessJwtToken;
+        _tokensManager.SaveRefreshToken(registerResponse.RefreshToken);
+    }
+}
---
+++ Finance_Manager_WPF_Front/Services/AuthServices/TokensManager.cs
@@ -0,0 +1,113 @@
+﻿using Finance_Manager_WPF_Front.BackendApi;
+using Finance_Manager_WPF_Front.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Finance_Manager_WPF_Front.Services.AuthServices;
+
+public class TokensManager
+{
+    private readonly IServiceProvider _provider; // Need for break cycle references with FinanceApiClient
+    private readonly UserSession _userSession;
+    private string _filePath;
+
+    public TokensManager(IServiceProvider provider, UserSession userSession)
+    {
+        var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        var dir = Path.Combine(appData, "My Finance Manager");
+        Directory.CreateDirectory(dir);
+        _filePath = Path.Combine(dir, "refresh_token.dat");
+        _provider = provider;
+        _userSession = userSession;
+        _userSession.LogoutAction += DeleteRefreshToken;
+    }
+
+    public async Task<string> GetAccessTokenAsync()
+    {
+        var token = _userSession.AccessToken;
+
+        if (token != null && IsAccessTokenActual(token))
+        {
+            return token;
+        }
+        else
+        {
+            var refresh = LoadRefreshToken();
+            if (refresh == null) return null;
+
+            var _apiClient = _provider.GetRequiredService<FinanceApiClient>();
+
+            var response = await _apiClient.RefreshTokenAsync(refresh);
+
+            SaveRefreshToken(response.RefreshToken);
+            _userSession.AccessToken = response.AccessJwtToken;
+
+            return response.AccessJwtToken;
+        }
+    }
+
+    // Copied to tests
+    private bool IsAccessTokenActual(string accessToken)
+    {
+        if (string.IsNullOrWhiteSpace(accessToken)) return false;
+
+        try
+        {
+            var parts = accessToken.Split('.');
+            if (parts.Length != 3)
+                return false;
+
+            var payload = parts[1];
+            // JWT payload is Base64Url encoded
+            payload = payload.PadRight(payload.Length + (4 - payload.Length % 4) % 4, '=');
+            var json = Encoding.UTF8.GetString(Convert.FromBase64String(payload));
+
+            var payloadData = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+            if (payloadData == null || !payloadData.TryGetValue("exp", out var expObj))
+                return false;
+
+            var exp = Convert.ToInt64(expObj);
+            var expirationTime = DateTimeOffset.FromUnixTimeSeconds(exp);
+            return expirationTime > DateTimeOffset.UtcNow;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public void SaveRefreshToken(string refreshToken)
+    {
+        var tokenBytes = Encoding.UTF8.GetBytes(refreshToken);
+        var encryptedBytes = ProtectedData.Protect(tokenBytes, null, DataProtectionScope.CurrentUser);
+
+        File.WriteAllBytes(_filePath, encryptedBytes);
+    }
+
+    public string? LoadRefreshToken()
+    {
+        try
+        {
+            var encryptedBytes = File.ReadAllBytes(_filePath);
+            var decryptedBytes = ProtectedData.Unprotect(encryptedBytes, null, DataProtectionScope.CurrentUser);
+            return Encoding.UTF8.GetString(decryptedBytes);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public void DeleteRefreshToken()
+    {
+        if (File.Exists(_filePath)) File.Delete(_filePath);
+    }
+}
