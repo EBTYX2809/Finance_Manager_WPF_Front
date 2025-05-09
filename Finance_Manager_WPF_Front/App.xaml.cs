320452932a91f1566aba96741f024736bad4ddd4
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-05-09 07:34:42 +0300

    DI and other changes.

--- Finance_Manager_WPF_Front/App.xaml.cs
+++ Finance_Manager_WPF_Front/App.xaml.cs
@@ -6,6 +6,9 @@ using System.Windows;
 using System.IO;
 using Finance_Manager_WPF_Front.BackendApi;
 using System.Net.Http;
+using Finance_Manager_WPF_Front.Services;
+using Finance_Manager_WPF_Front.Models;
+using Finance_Manager_WPF_Front.Services.AuthServices;
 
 namespace Finance_Manager_WPF_Front
 {
@@ -23,16 +26,36 @@ namespace Finance_Manager_WPF_Front
             ConfigureServices(services);
             ServiceProvider = services.BuildServiceProvider();
 
-            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
-            mainWindow.Show();
+            /*var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();*/
+
+            var loginWindow = ServiceProvider.GetRequiredService<LoginWindow>();
+            loginWindow.Show();
         }
 
         private void ConfigureServices(IServiceCollection services)
         {
-            //services.AddSingleton<UserSession>();
+            // Helpers
+            services.AddAutoMapper(typeof(AutoMapperProfile));
+            services.AddSingleton<UserSession>();            
 
+            // API
             services.AddSingleton<HttpClient>();
+            services.AddSingleton<ApiWrapper>();
+            services.AddSingleton<FinanceApiClient>(sp =>
+            {
+                var baseUrl = "https://localhost:49464";
+                var httpClient = sp.GetRequiredService<HttpClient>();
+                var tokenManager = sp.GetRequiredService<TokensManager>();
+                return new FinanceApiClient(baseUrl, httpClient, tokenManager);
+            });
 
+            // Services
+            services.AddSingleton<AuthService>();
+            services.AddSingleton<TokensManager>();
+            services.AddSingleton<UserService>();
+
+            // ViewModels
             services.AddSingleton<LoginViewModel>();
             services.AddSingleton<TransactionsViewModel>();
             services.AddSingleton<AnalyticsViewModel>();
@@ -40,8 +63,10 @@ namespace Finance_Manager_WPF_Front
             services.AddSingleton<PlanningViewModel>();
             services.AddSingleton<SettingsViewModel>();
 
+            // Views
             services.AddSingleton<LoginWindow>();
             services.AddSingleton<MainWindow>();
+            services.AddSingleton<WindowChanger>();
         }      
     }
 }
