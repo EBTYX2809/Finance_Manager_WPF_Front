adc594b231185207403ecd6c70b2d50d3018a496
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-05-10 22:17:51 +0300

    Add GetCategoryById method.

--- Finance_Manager_WPF_Front/Models/CategoriesStorage.cs
+++ Finance_Manager_WPF_Front/Models/CategoriesStorage.cs
@@ -3,4 +3,9 @@
 public static class CategoriesStorage
 {
     public static List<CategoryModel> AllCategories { get; set; } = new();
+
+    public static CategoryModel? GetCategoryById(int id)
+    {
+        return AllCategories.FirstOrDefault(c => c.Id == id);
+    }
 }
