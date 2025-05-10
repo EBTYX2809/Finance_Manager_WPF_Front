aaaf690ddb01fc4e6f24d777c88dbed8c8b492aa
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-05-10 23:54:09 +0300

    Updated mapper.

--- Finance_Manager_WPF_Front/Models/AutoMapperProfile.cs
+++ Finance_Manager_WPF_Front/Models/AutoMapperProfile.cs
@@ -5,11 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using Finance_Manager_WPF_Front.BackendApi;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Finance_Manager_WPF_Front.Models;
 
 public class AutoMapperProfile : Profile
-{
+{   
     public AutoMapperProfile()
     {
         CreateMap<CurrencyBalanceDTO, UserCurrencyBalanceModel>();
@@ -20,13 +21,20 @@ public class AutoMapperProfile : Profile
             .ForMember(dest => dest.Date, opt => opt.MapFrom(src =>
             TimeZoneInfo.ConvertTimeFromUtc(src.Date.DateTime, TimeZoneInfo.Local)))
             .ForMember(dest => dest.Category, opt => opt.MapFrom(src =>
-            CategoriesStorage.GetCategoryById(src.CategoryId)));
-        CreateMap<TransactionModel, TransactionDTO>();
+            CategoriesStorage.GetCategoryById(src.CategoryId)))
+            .ForMember(dest => dest.InnerCategory, opt => opt.MapFrom(src =>
+            CategoriesStorage.GetCategoryById(src.InnerCategoryId ?? 0)));
+
+        CreateMap<TransactionModel, TransactionDTO>()
+            .ForMember(dest => dest.Date, opt => opt.MapFrom(src =>
+            TimeZoneInfo.ConvertTimeToUtc(src.Date.Date, TimeZoneInfo.Local)))
+            .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.Category.Id))
+            .ForMember(dest => dest.InnerCategoryId, opt => opt.MapFrom(src => src.InnerCategory.Id));            
 
         CreateMap<SavingDTO, SavingModel>();
-        CreateMap<SavingModel,SavingDTO>();
+        CreateMap<SavingModel, SavingDTO>();            
 
         CreateMap<CategoryDTO, CategoryModel>();
-        CreateMap<CategoryModel, CategoryDTO>();
+        CreateMap<CategoryModel, CategoryDTO>(); // Мб добавить маппинг для ParentCategoryId        
     }
 }
