86623236d7a32396394b90bfafcf3fc2c80d8600
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-05-12 11:05:20 +0300

    Created functional for view models switching in side bar.

--- Finance_Manager_WPF_Front/Views/MainWindow.xaml.cs
+++ Finance_Manager_WPF_Front/Views/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System.Net.Http;
 using System.Windows;
+using System.Windows.Input;
 using Finance_Manager_WPF_Front.BackendApi;
 using Finance_Manager_WPF_Front.Models;
 using Finance_Manager_WPF_Front.Services;
+using Finance_Manager_WPF_Front.ViewModels;
 
 namespace Finance_Manager_WPF_Front.Views;
 
@@ -16,7 +18,11 @@ public partial class MainWindow : Window
     private readonly CategoriesService _categoriesService;
     private readonly TransactionsService _transactionsService;
     private readonly SavingsService _savingsService;
-    public MainWindow(UserSession userSession, UserService userService, CategoriesService categoriesService, TransactionsService transactionsService, SavingsService savingsService)
+    private readonly TransactionsViewModel _transactionsViewModel;
+    private readonly SavingsViewModel _savingsViewModel;
+    private readonly AnalyticsViewModel _analyticsViewModel;
+    private readonly SettingsViewModel _settingsViewModel;
+    public MainWindow(UserSession userSession, UserService userService, CategoriesService categoriesService, TransactionsService transactionsService, SavingsService savingsService, TransactionsViewModel transactionsViewModel, SavingsViewModel savingsViewModel, AnalyticsViewModel analyticsViewModel, SettingsViewModel settingsViewModel)
     {
         InitializeComponent();
         _userSession = userSession;
@@ -25,7 +31,13 @@ public partial class MainWindow : Window
         _transactionsService = transactionsService;
         _savingsService = savingsService;
 
+        _transactionsViewModel = transactionsViewModel;
+        _savingsViewModel = savingsViewModel;
+        _analyticsViewModel = analyticsViewModel;
+        _settingsViewModel = settingsViewModel;
+
         Loaded += MainWindow_Loaded;
+        //TransactionsPageButton_Click(null, null);
     }
 
     private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
@@ -40,5 +52,41 @@ public partial class MainWindow : Window
         await _categoriesService.LoadAllCategoriesAsync();
         await _transactionsService.GetTransactionsPageAsync();
         await _savingsService.GetSavingsPageAsync();       
+    }
+
+    private void TransactionsPageButton_Click(object sender, RoutedEventArgs e)
+    {
+        DataContext = _transactionsViewModel;
+        HidePages();
+        //TransactionsPage.Visibility = Visibility.Visible;
+    }
+
+    private void SavingsPageButton_Click(object sender, RoutedEventArgs e)
+    {
+        DataContext = _savingsViewModel;
+        HidePages();
+        //SavingsPage.Visibility = Visibility.Visible;
+    }
+
+    private void AnalyticsPageButton_Click(object sender, RoutedEventArgs e)
+    {
+        DataContext = _analyticsViewModel;
+        HidePages();
+        //AnalyticsPage.Visibility = Visibility.Visible;
+    }
+
+    private void SettigsPageButton_Click(object sender, RoutedEventArgs e)
+    {
+        DataContext = _settingsViewModel;
+        HidePages();
+        //SettigsPage.Visibility = Visibility.Visible;
+    }
+
+    private void HidePages()
+    {
+/*        TransactionsPage.Visibility = Visibility.Collapsed;
+        SavingsPage.Visibility = Visibility.Collapsed;
+        AnalyticsPage.Visibility = Visibility.Collapsed;
+        SettigsPage.Visibility = Visibility.Collapsed;*/
     }
 }
