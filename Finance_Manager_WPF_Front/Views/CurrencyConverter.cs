724f5473e1607bdf4b802df041cc639e805600b5
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-05-16 16:12:07 +0300

    Updated output money with correct currency from user culture.

---
+++ Finance_Manager_WPF_Front/Views/CurrencyConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Finance_Manager_WPF_Front.Views;
+
+public class CurrencyConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+            return "";
+
+        if (!(value is IFormattable formattable))
+            return value.ToString() ?? "";
+
+        CultureInfo ci;
+        try
+        {
+            ci = CurrencyCultureProvider.GetCultureFromUserPrimaryCurrency();
+        }
+        catch
+        {
+            ci = CultureInfo.InvariantCulture;
+        }
+
+        return formattable.ToString("C", ci);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException("Reverse transformation is not supported");
+    }
+}
+
+
---
+++ Finance_Manager_WPF_Front/Views/CurrencyCultureProvider.cs
@@ -0,0 +1,56 @@
+﻿using Finance_Manager_WPF_Front.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance_Manager_WPF_Front.Views;
+
+public static class CurrencyCultureProvider
+{
+    private static UserSession _userSession;
+
+    // Хардкод словаря популярных валют и регионов
+    private static readonly Dictionary<string, string> _defaultCurrencyCultures = new(StringComparer.OrdinalIgnoreCase)
+    {
+        { "UAH", "uk-UA" }, // Гривны - Украина
+        { "USD", "en-US" }, // Доллары - США
+        { "RUB", "ru-RU" }, // Рубли - Россия
+        { "EUR", "de-DE" }, // Евро - Европа (выбрал Германия как пример)
+        { "PLN", "pl-PL" }, // Злотые - Польша
+        { "GBP", "en-GB" }, // Фунты стерлингов - Великобритания
+        { "JPY", "ja-JP" }, // Иена - Япония
+        { "CNY", "zh-CN" }, // Юань - Китай
+        { "INR", "hi-IN" }, // Индийская рупия - Индия
+        { "CAD", "en-CA" }, // Канадский доллар - Канада
+        { "AUD", "en-AU" }, // Австралийский доллар - Австралия
+        { "CHF", "de-CH" }, // Швейцарский франк - Швейцария
+        { "NZD", "en-NZ" }, // Новозеландский доллар - Новая Зеландия
+        { "SEK", "sv-SE" }, // Шведская крона - Швеция
+        { "KRW", "ko-KR" }, // Южнокорейская вона - Южная Корея
+    };
+
+    public static void Initialize(UserSession userSession)
+    {
+        if (_userSession != null)
+            throw new InvalidOperationException("CurrencyCultureProvider is already initialized.");
+
+        _userSession = userSession ?? throw new ArgumentNullException(nameof(userSession));
+    }
+
+    public static CultureInfo GetCultureFromUserPrimaryCurrency()
+    {       
+        var currencyCode = _userSession.CurrentUser.PrimaryCurrencyBalance.Currency;
+
+        var cultureName = _defaultCurrencyCultures[currencyCode];
+
+        return new CultureInfo(cultureName);
+    }
+
+    public static List<string> GetCurrenciesList()
+    {
+        return _defaultCurrencyCultures.Keys.ToList();
+    }
+}
