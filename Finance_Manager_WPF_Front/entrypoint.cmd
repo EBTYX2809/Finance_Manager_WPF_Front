c20ce8b7f09a4f4a30d9f6e16d7ad8d2cf54d023
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-07-03 20:43:40 +0300

    Deleted --single-file parameter in entrypoint command.

--- Finance_Manager_WPF_Front/entrypoint.cmd
+++ Finance_Manager_WPF_Front/entrypoint.cmd
@@ -2,7 +2,6 @@
 netsparkle-generate-appcast ^
   --appcast-output-directory C:/out ^
   --binaries C:/app/publish ^
-REM  --single-file C:/app/FinanceManagerWPF.zip ^
   --ext exe ^
   --os windows-x64 ^
   --base-url %APP_BASE_URL% ^
