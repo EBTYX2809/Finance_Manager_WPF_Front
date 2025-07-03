b634ad853044067785f4bb8bbca5e67e7d076552
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-07-03 16:46:21 +0300

    Fixed url on binary file in appcast.xml. Added archivation /publish folder.

--- Finance_Manager_WPF_Front/entrypoint.cmd
+++ Finance_Manager_WPF_Front/entrypoint.cmd
@@ -1,9 +1,11 @@
 @echo off
 netsparkle-generate-appcast ^
   --appcast-output-directory C:/out ^
-  --binaries C:/app/publish ^
-  --ext exe ^
+  --binaries C:/app
+  --single-file C:/app/FinanceManagerWPF.zip ^
+  --ext zip ^
   --os windows-x64 ^
+  --base-url %APP_BASE_URL%
   --product-name FinanceManagerWPF ^
   --human-readable true ^
   --output-file-name appcast
