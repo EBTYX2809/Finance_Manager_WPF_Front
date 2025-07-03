0dc6dd5fbd35c7ba88f3f94bf48c8441e82e3832
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-07-03 20:36:25 +0300

    Commented --single-file parameter in entrypoint command.

--- Finance_Manager_WPF_Front/entrypoint.cmd
+++ Finance_Manager_WPF_Front/entrypoint.cmd
@@ -2,7 +2,7 @@
 netsparkle-generate-appcast ^
   --appcast-output-directory C:/out ^
   --binaries C:/app/publish ^
-  --single-file C:/app/FinanceManagerWPF.zip ^
+REM  --single-file C:/app/FinanceManagerWPF.zip ^
   --ext exe ^
   --os windows-x64 ^
   --base-url %APP_BASE_URL% ^
