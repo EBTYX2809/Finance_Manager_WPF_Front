e92419a53f2d58b067a688f8c3f383b414cd348b
Artem Yevtushenko <ebtyx2809@gmail.com>
2025-07-03 17:05:16 +0300

    Fixed path for archivation.

--- Finance_Manager_WPF_Front/entrypoint.cmd
+++ Finance_Manager_WPF_Front/entrypoint.cmd
@@ -5,7 +5,7 @@ netsparkle-generate-appcast ^
   --single-file C:/app/FinanceManagerWPF.zip ^
   --ext zip ^
   --os windows-x64 ^
-  --base-url %APP_BASE_URL%
+  --base-url %APP_BASE_URL% ^
   --product-name FinanceManagerWPF ^
   --human-readable true ^
   --output-file-name appcast
